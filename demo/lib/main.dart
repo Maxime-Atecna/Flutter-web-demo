1118e2f9e33b27481074f68408a8f195cf2e1bcc
Maxime-Atecna <100686301+Maxime-Atecna@users.noreply.github.com>
2024-08-15 11:10:51 +0200

    add urlstrategy and scope

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'core/router/app_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  setPathUrlStrategy();
+  GoRouter.optionURLReflectsImperativeAPIs = true;
+  runApp(const ProviderScope(
+    child: MyApp(),
+  ));
 }
 
 class MyApp  extends ConsumerWidget {
